bda3ca2b0bd4743bc0f621ff2804904eaa101c34
EC2 Default User <ec2-user@ip-172-31-168-79.ec2.internal>
2022-08-08 19:40:42 +0000

    lab4.1

--- workshop2/creds.sh
+++
@@ -1,4 +0,0 @@
-cat << EoF > clo835_creds.sh
-export AWS_SECRET_ACCESS_KEY=$(jq -r .AccessKey.SecretAccessKey /tmp/create_output.json)
-export AWS_ACCESS_KEY_ID=$(jq -r .AccessKey.AccessKeyId /tmp/create_output.json)
-EoF
